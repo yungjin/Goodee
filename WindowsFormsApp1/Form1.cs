aa9c2ec9ea5dd608e7085bf906e78a137b414eac
영진 최 <chyj0318@naver.com>
2018-11-12 10:59:24 +0900

    button 코드로 생성하기

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,9 +12,31 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1()          //생성자
         {
             InitializeComponent();
+            Load += Form1_Load; // 생성자가 만들어 질때 생성괴는것
+            
         }
+
+        private void Form1_Load(object sender, EventArgs e) //매서드
+        {
+            Button btn = new Button();
+
+            btn.DialogResult = DialogResult.OK;
+            btn.Text = "확인";
+            btn.Size = new Size(100, 50);     // 버튼 사이즈 
+            btn.Location = new Point(350, 30);// 버튼 위치점
+
+            Controls.Add(btn);
+            btn.Click += btn_click;
+        }
+
+        private void btn_click(object o , EventArgs a)
+        {
+            MessageBox.Show("확인하기");
+        }
+
+
     }
 }
