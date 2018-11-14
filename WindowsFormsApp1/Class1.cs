9c03ad57975d90f35e3d0fafd39e6d96c5eed3e0
영진 최 <chyj0318@naver.com>
2018-11-14 10:52:15 +0900

    클래스 에서 버튼 만들기

---
+++ WindowsFormsApp1/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Class1
+    {
+        Form form;
+        public Class1(Form form)
+        {
+            this.form = form;
+
+
+        }
+
+        public void btn(Form form)
+        {
+            Button btn = new Button();
+
+            btn.DialogResult = DialogResult.OK;
+            btn.Name = string.Format("btn_{0}", (1));
+            btn.Text = string.Format("확인 : {0}", (1));
+            btn.Size = new Size(100, 50);     // 버튼 사이즈 
+            btn.Location = new Point(30, 30);// 버튼 위치점
+            btn.Cursor = Cursors.Hand; //마우스 손모양으로 바꾸기
+
+            form.Controls.Add(btn);
+            
+        }
+
+    }
+}
